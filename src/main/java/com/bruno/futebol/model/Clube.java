234b1a026f2086b2292c26a66e03b5474b09f2a3
Bruno Neves <brunonevesufrj@gmail.com>
2020-04-21 20:07:59 -0300

    Adicionando entidade Clube

---
+++ src/main/java/com/bruno/futebol/model/Clube.java
@@ -0,0 +1,66 @@
+package com.bruno.futebol.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Clube {
+
+    @Id
+    private UUID clubeId;
+
+    @Column(length = 5)
+    @Size(min = 2, max = 5)
+    private String sigla;
+
+    private String nomeCurto;
+
+    @Column(unique = true)
+    private String nomeLongo;
+
+    private LocalDate fundacao;
+
+    public UUID getClubeId() {
+        return clubeId;
+    }
+
+    public void setClubeId(UUID clubeId) {
+        this.clubeId = clubeId;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNomeCurto() {
+        return nomeCurto;
+    }
+
+    public void setNomeCurto(String nomeCurto) {
+        this.nomeCurto = nomeCurto;
+    }
+
+    public String getNomeLongo() {
+        return nomeLongo;
+    }
+
+    public void setNomeLongo(String nomeLongo) {
+        this.nomeLongo = nomeLongo;
+    }
+
+    public LocalDate getFundacao() {
+        return fundacao;
+    }
+
+    public void setFundacao(LocalDate fundacao) {
+        this.fundacao = fundacao;
+    }
+}
